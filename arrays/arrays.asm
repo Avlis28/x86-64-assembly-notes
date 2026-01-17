bb893acb7e7f329902e2043094dcb9df75b13b25
sandbox <ghostnether28@gmail.com>
2026-01-17 20:14:09 -0300

    adicinar pasta de arrays, e padrao 16 bits nos registradores

---
+++ arrays/arrays.asm
@@ -0,0 +1,14 @@
+%include "bibliotecaE.asm"
+
+section .data
+    msg1: db "Parte 1", lf, null
+    msg2: db "Parte 2", lf, null
+    msg3: db "Parte 3", lf, null
+    msg4: db "Parte 4", lf, null
+
+section .text 
+
+
+global _start 
+
+_start:
